338302468c24acd8b8959ffc7882cb9520b4aa0b
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2022-02-18 07:19:17 +0200

    add themaQuiz show questions functionality

--- src/components/ThemeQuiz/ThemaQuiz.js
+++ src/components/ThemeQuiz/ThemaQuiz.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import st from './ThemaQuiz.module.css';
+import { useSelector } from 'react-redux';
 
 export default function ThemaQuiz() {
+  const { quiz, thema } = useSelector(state => state.quiz);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const themaQuestions = quiz.find(item => item.name === thema);
+  const nextQuestion = () => {
+    setQuestionNumber(questionNumber + 1);
+  };
   return (
-    <div>Thema Quiz</div>
+    <div className={st.wrapper}>
+      <h3 className={st.title}>{thema}</h3>
+      <p>{themaQuestions.questions[questionNumber].question}</p>
+      {questionNumber >= themaQuestions.questions.length-1
+        ? <button>Show Result</button>
+        : <button onClick={nextQuestion}>Next question</button>}
+    </div>
   )
 }
